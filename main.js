f15483628fc7bd515d75a0134c1b502cba238b60
Andy McWilliams <andy@jahya.net>
2014-07-18 01:41:30 -0400

    Allows skipping between weeks

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+var mondayCurrentlyDisplayed = new Date;
+
 $( document ).ready(function() {
-  $(".scrollable").scrollTop(490);
+  init();
+
+  $(".scrollable").scrollTop(470);
+
+  $("#back").click(function() {
+    decrementWeek();
+    updateSources();
+  });
+
+  $("#next").click(function() {
+    incrementWeek();
+    updateSources();
+  });
 });
+
+function init(){
+  var today = new Date;
+  var diff = today.getDate() - today.getDay() + 1;
+  mondayCurrentlyDisplayed = new Date(today.setDate(diff));
+}
+
+function incrementWeek(){
+  mondayCurrentlyDisplayed.setDate(mondayCurrentlyDisplayed.getDate() + 7);
+}
+
+function decrementWeek(){
+  mondayCurrentlyDisplayed.setDate(mondayCurrentlyDisplayed.getDate() - 7);
+}
+
+function updateSources(){
+  $("#jason").attr("src", getSrc(5117024));
+  $("#andy" ).attr("src", getSrc(5116706));
+  $("#mark" ).attr("src", getSrc(5135253));
+}
+
+function getSrc(userId){
+  return "https://www.toggl.com/app/reports/summary/612214/from/"
+    + moment(mondayCurrentlyDisplayed).format("YYYY-MM-DD")
+    + "/to/"
+    + moment(mondayCurrentlyDisplayed).add('days', 6).format("YYYY-MM-DD")
+    + "/projects/"
+    + userId
+    + "/billable/both";
+}
